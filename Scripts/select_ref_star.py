14c6962603553c822ce1b425abc0a709b99a559a
Nicholas Phillips <ntp28@cornell.edu>
2025-10-12 11:20:18 -0600

    Built inputs, outputs and docstring

---
+++ Scripts/select_ref_star.py
@@ -0,0 +1,11 @@
+import corgietc as ct  # noqa
+import os
+import json
+import EXOSIMS.Prototypes.TargetList
+import EXOSIMS.Prototypes.TimeKeeping
+import copy
+import astropy.units as u
+import pandas as pd
+import roman_pointing as rp
+import corgisim as cs
+import corgidb as cd 
